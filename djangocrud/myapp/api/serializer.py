7074e2d1bfac5b417c3a88f56f65831eb7fcfd94
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-07-08 09:29:48 -0500

    Instalar Cors-Headers para que Django permita las solicitudes de Django, Además se configuro en Django el archivo settings.py para que funcione con dicho modulo.Se definio la url o servidor que se le va a permitir las solicitudes

--- djangocrud/myapp/api/serializer.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User,Group
-from rest_framework import serializers
-from .models import Movie
-
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = ('id','title','desc','year')
